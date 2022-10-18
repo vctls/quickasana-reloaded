d72830b76237945f6dea67525e1bee3b923721a1
Ian Gulliver <git@flamingcow.io>
2022-10-17 23:35:53 -0700

    Fast tab close, upload screenshot attachment

--- background.js
+++ background.js
@@ -1,9 +1,13 @@
+const reData = new RegExp('^data:(.*?);base64,(.*)$');
+
 async function handleClick() {
 	const tabs = await browser.tabs.query({currentWindow: true, active: true});
 	if (tabs.length != 1) {
 		return;
 	}
 	const tab = tabs[0];
+
+	const promiseImgURL = browser.tabs.captureTab(tab.id);
 
 	const cfg = await browser.storage.sync.get();
 	const req = {
@@ -15,8 +19,6 @@ async function handleClick() {
 		},
 	};
 	console.log('-->', req);
-
-	const promiseImg = browser.tabs.captureTab(tab.id);
 
 	const promiseCreateResp = fetch(
 		'https://app.asana.com/api/1.0/tasks',
@@ -31,13 +33,42 @@ async function handleClick() {
 		},
 	);
 
-	const img = await promiseImg;
+	const imgURL = await promiseImgURL;
+	const [_, imgType, imgBase64] = imgURL.match(reData);
+	const img = atob(imgBase64);
+
+	// All tab info captured
+	browser.tabs.remove([tab.id]);
+
 	const createResp = await promiseCreateResp;
 	const create = await createResp.json();
 
 	console.log('<--', create);
 
-	browser.tabs.remove([tab.id]);
+	const imgBytes = new Uint8Array(img.length);
+	for (let i = 0; i < img.length; i++) {
+		imgBytes[i] = img.charCodeAt(i);
+	}
+	const blob = new Blob(
+		[imgBytes],
+		{
+			type: imgType,
+		},
+	);
+
+	const data = new FormData();
+	data.append('file', blob, 'screenshot.png');
+
+	const attachResp = await fetch(
+		`https://app.asana.com/api/1.0/tasks/${encodeURIComponent(create['data']['gid'])}/attachments`,
+		{
+			method: 'POST',
+			headers: {
+				'Authorization': `Bearer ${cfg['token']}`,
+			},
+			body: data,
+		},
+	);
 }
 
 browser.browserAction.onClicked.addListener(handleClick);
